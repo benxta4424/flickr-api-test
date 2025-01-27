573d428f963ce74c276fc0d7f0dc5708511b0ad9
benxta4424 <benxtaben@gmail.com>
2025-01-27 22:36:25 +0200

    adding a rescue block in case there is no user found

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,12 @@ class UsersController < ApplicationController
   require "flickr"
   before_action :set_flickr
   def index
-    unless params[:user_id].nil?
-      @photos=flickr.photos.search(user_id: params[:user_id])
+    begin
+      if !params[:user_id].nil?
+        @photos=@flickr.photos.search(user_id: params[:user_id])
+      end
+    rescue Flickr::FailedResponse
+      flash[:error] = "Failed to fetch photos. Please check the User ID."
     end
   end
 
