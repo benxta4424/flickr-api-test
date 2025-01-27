d0d826c63f8d0acae35c7d301661f42835a3ef76
benxta4424 <benxtaben@gmail.com>
2025-01-27 21:57:57 +0200

    creating the controller for the handling of the search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  require "flickr"
+  before_action :set_flickr
+  def index
+    unless params[:user_id].nil?
+      @photos=flickr.photos.search(user_id: params[:user_id])
+    end
+  end
+
+  def set_flickr
+    @flickr=Flickr.new(Figaro.env.flickr_api_key, Figaro.env.flickr_api_secret_key)
+  end
 end
